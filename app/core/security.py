a7d4223aabe1f7c1dcbd481e5d1285604d48fcd7
ram2005024 <sharmashekhar20050@gmail.com>
2026-06-22 12:47:57 +0545

    Added the RBAC in Project Endpoint

--- app/core/security.py
+++ app/core/security.py
@@ -1,8 +1,6 @@
 from datetime import datetime, timedelta
 
-from fastapi import Request, status
-from fastapi.responses import JSONResponse
-from jose import ExpiredSignatureError, JWTError, jwt
+from jose import jwt
 from pwdlib import PasswordHash
 
 from app.core.config import redis, settings
@@ -42,47 +40,3 @@ def blacklist_token(token: str, exp: int) -> bool:
 # Check the blacklist token
 def isblacklisted(token: str):
     return redis.exists(f"blacklist-{token}")
-
-
-# Blacklist middleware
-async def blacklist_middleware(request: Request, call_next):
-    pass_path = [
-        "/auth/login",
-        "/auth/register",
-        "/auth/refresh",
-        "/auth/logout",
-        "/docs",
-        "/redoc",
-        "/openapi.json",
-    ]
-    if request.url.path in pass_path:
-        return await call_next(request)
-    auth_header = request.headers.get("Authorization")
-    if not auth_header:
-        return JSONResponse(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            content={"detail": "Header is missing"},
-        )
-    token = auth_header.split(" ")[1]
-    # Verify if the token is blacklisted or not
-    if isblacklisted(token):
-        return JSONResponse(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            content={"detail": "Token revoked"},
-        )
-    try:
-        payload = jwt.decode(
-            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
-        )
-    except ExpiredSignatureError:
-        return JSONResponse(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            content={"detail": "Expired token"},
-        )
-    except JWTError:
-        return JSONResponse(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            content={"detail": "Invalid token"},
-        )
-    request.state.user_id = payload["id"]
-    return await call_next(request)
